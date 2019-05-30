b1f2f1af9f6ffb59428ad17d149361c75e34b662
Manny Georganas <manoli.georganas@gmail.com>
2019-05-30 10:11:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 def position_taken?(board, index_number)
   if board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
     return false
+  elsif board[index_number] == "X" || board[index_number] == "O"
+    return true
   end
 end
